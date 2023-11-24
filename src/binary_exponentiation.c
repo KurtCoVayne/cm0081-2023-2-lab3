6ed6129628ab1d340f00e45bf2bc2ce11defdfe3
John Gonzalez <john.gonzalez@ideaslab.com.co>
2023-11-23 23:24:53 -0500

    returned the code as the way it should really is

--- src/binary_exponentiation.c
+++ src/binary_exponentiation.c
@@ -1,13 +1,6 @@
-// long binary_exponentiation(long b, long e) {
-//   long ans = 1;
-// 	for (; e; b = b*b, e >>= 1)
-// 		if (e&1) ans = ans*b;
-// 	return ans;
-// }
-// Do it recursively:
-long binary_exponentiation(long *ans, long b, long e) {
-  if (e == 0) return 1;
-  binary_exponentiation(ans, b*b, e>>1);
-  if (e&1) *ans = *ans*b;
-  return *ans;
+long binary_exponentiation(long b, long e) {
+  long ans = 1;
+	for (; e; b = b*b, e >>= 1)
+		if (e&1) ans = ans*b;
+	return ans;
 }
